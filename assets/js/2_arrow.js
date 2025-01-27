20c70cf021016fbfeff04b0d5a589767aec660dd
Paulacmcm <ps.paulacortesmonroy@gmail.com>
2025-01-27 15:17:32 -0300

    cambios

--- assets/js/2_arrow.js
+++ assets/js/2_arrow.js
@@ -1,1 +1,10 @@
+let suma = function(a, b){
+    return a + b
+    }
+    
+
+
+
+
+
 let suma = (a, b) => a + b;
--- assets/js/colores.js
+++ assets/js/colores.js
@@ -1,4 +1,4 @@
-// Evento para cambiar de color a negro al hacer click en los divs con colores iniciales
+
 const divs = ['blue', 'red', 'green', 'yellow'];
 
 divs.forEach(id => {
@@ -8,39 +8,39 @@ divs.forEach(id => {
   });
 });
 
-// Variable global para guardar el color basado en la tecla presionada
+
 let colorGuardado = '';
 
-// Evento para cambiar el color del div 'key' según la tecla presionada
+
 document.addEventListener('keydown', function (event) {
   if (event.key === 'a') {
-    colorGuardado = 'pink'; // Rosado
+    colorGuardado = 'pink'; 
     document.getElementById('key').style.backgroundColor = colorGuardado;
   } else if (event.key === 's') {
-    colorGuardado = 'orange'; // Naranjo
+    colorGuardado = 'orange'; 
     document.getElementById('key').style.backgroundColor = colorGuardado;
   } else if (event.key === 'd') {
-    colorGuardado = 'cyan'; // Celeste
+    colorGuardado = 'cyan'; 
     document.getElementById('key').style.backgroundColor = colorGuardado;
   } else if (event.key === 'q') {
     const newDiv = document.createElement('div');
     newDiv.style.width = '200px';
     newDiv.style.height = '200px';
-    newDiv.style.backgroundColor = 'purple'; // Morado
+    newDiv.style.backgroundColor = 'purple'; 
     newDiv.style.border = '2px solid black';
     document.body.appendChild(newDiv);
   } else if (event.key === 'w') {
     const newDiv = document.createElement('div');
     newDiv.style.width = '200px';
     newDiv.style.height = '200px';
-    newDiv.style.backgroundColor = 'gray'; // Gris
+    newDiv.style.backgroundColor = 'gray'; 
     newDiv.style.border = '2px solid black';
     document.body.appendChild(newDiv);
   } else if (event.key === 'e') {
     const newDiv = document.createElement('div');
     newDiv.style.width = '200px';
     newDiv.style.height = '200px';
-    newDiv.style.backgroundColor = 'brown'; // Café
+    newDiv.style.backgroundColor = 'brown'; 
     newDiv.style.border = '2px solid black';
     document.body.appendChild(newDiv);
   }
